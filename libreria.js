072eaeadeeb37275219e2335de27ecb46d04b33f
AgustinItuarte <agustin242536@gmail.com>
2022-12-20 15:50:04 -0300

    Atributo data añadido con index de libro

--- libreria.js
+++ libreria.js
@@ -1,33 +1,36 @@
 const mostrarLibros = document.querySelector('.libros');
-const añadirLibros = document.querySelector('.añadirlibro-btn')
-const form = document.querySelector('.añadir-libro')
-const enviarForm = document.querySelector('.enviar-form')
+const añadirLibros = document.querySelector('.añadirlibro-btn');
+const form = document.querySelector('.añadir-libro');
+const enviarForm = document.querySelector('.enviar-form');
 
 let myLibrary = [];
 
-function Book(titulo, autor, paginas, leido) {
+function Book(titulo, autor, paginas, leido, nº) {
     this.Titulo = titulo
     this.Autor = autor
     this.Paginas = paginas
     this.Leido = leido
+    this.LibroNº = nº;
 }
 
 function addBookToLibrary() {
+    let ultimoElem2 = myLibrary.length - 1
     let titulo = form.elements[0].value;
     let autor = form.elements[1].value;
     let cantPag = form.elements[2].value;
     let leido = form.elements[3].value;
     
-    libro = new Book(titulo, autor, cantPag, leido,)
+    libro = new Book(titulo, autor, cantPag, leido, ultimoElem2 + 1)
     myLibrary.push(libro);
 
-    console.log(myLibrary);
-
     //Muestra libros en pantalla.
-
+    
     let ultimoElem = myLibrary.length - 1
     let btnEliminar = document.createElement('button');
+    btnEliminar.className = 'btn-eliminar'
+    btnEliminar.dataset.id = `${ultimoElem}`;
     let div = document.createElement('div');
+    div.dataset.id = `${ultimoElem}`;
     mostrarLibros.appendChild(div);
     div.appendChild(btnEliminar);
     btnEliminar.innerHTML = 'ELIMINAR'
@@ -42,6 +45,17 @@ function addBookToLibrary() {
     }
 }
 
+function eliminarLibro(libro) {
+    const btn = document.querySelector('.btn-eliminar');
+    libro.id
+}
+
+if (btnEliminar) {
+    btnEliminar.addEventListener('click', () => {
+        console.log('hola')
+    })
+}
+    
 //Muestra form para añadir libros.
 añadirLibros.addEventListener('click', () => {
     form.hidden = false;
