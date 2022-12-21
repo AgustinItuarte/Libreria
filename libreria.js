bc299812665a2d2c2e8bd1da2aab85c8642990c6
AgustinItuarte <agustin242536@gmail.com>
2022-12-20 23:10:22 -0300

    funcion para cambiar estado de leido agregada

--- libreria.js
+++ libreria.js
@@ -2,6 +2,7 @@ const mostrarLibros = document.querySelector('.libros');
 const añadirLibros = document.querySelector('.añadirlibro-btn');
 const form = document.querySelector('.añadir-libro');
 const enviarForm = document.querySelector('.enviar-form');
+const check = document.querySelector('.check');
 
 let myLibrary = [];
 
@@ -11,23 +12,35 @@ function Book(titulo, autor, paginas, leido, nº) {
     this.Paginas = paginas
     this.Leido = leido
     this.LibroNº = nº;
+    this.estado = function() {
+        /* if (check.checked === true) {
+            return this.Leido = 'Si';
+        } */
+
+        if (this.Leido === 'No') {
+            return this.Leido = 'Si';
+        } else if (this.Leido === 'Si'){
+            return this.Leido = 'No';
+        }
+    }
 }
 
-//Añade los libros al array myLibrary y los muesta en pantalla
+//Añade los libros al array myLibrary y los muestra en pantalla
 function añadiraLibreria() {
     let check = document.querySelector('.check')
     let ultimoElem2 = myLibrary.length - 1
     let titulo = form.elements[0].value;
     let autor = form.elements[1].value;
     let cantPag = form.elements[2].value;
-    let leido = ''
+    let leido = form.elements[3].value;
+    /* leido = ''
 
     if (check.checked === true) {
         leido = 'Si';
     } else {
         leido = 'No';
     }
-
+ */
     libro = new Book(titulo, autor, cantPag, leido, ultimoElem2 + 1)
     myLibrary.push(libro);
     crearLibros()
@@ -41,6 +54,7 @@ function crearLibros() {
     btnEliminar.dataset.id = `${ultimoElem}`;
     let btnCheck = document.createElement('button');
     btnCheck.className = 'btn-check';
+    btnCheck.dataset.id = `${ultimoElem}`;
     let div = document.createElement('div');
     div.dataset.id = `${ultimoElem}`;
     mostrarLibros.appendChild(div);
@@ -54,6 +68,7 @@ function crearLibros() {
     
     for (let j = 0; j < llaves.length; j++) {
         let p = document.createElement('p');
+        p.className = `p${ultimoElem}`;
         div.appendChild(p);
         p.innerHTML = `${llaves[j]}: <br> ${valores[j]}`
     }
@@ -69,7 +84,39 @@ function eliminarLibro(evento) {
     }
 }
 
-//Evento que llama a funcion para eliminar libros
+
+//Cambia el estado de los libros de leido a no leido.
+function cambiarEstado(evento1) {
+    const elemento = evento1.target;
+
+    if (elemento.classList.contains('btn-check')) {
+        let id = elemento.dataset.id
+        let libro = myLibrary[id];
+        libro.estado();
+        
+        const div = document.querySelector(`div[data-id='${id}']`);
+        const p = document.querySelectorAll(`.p${id}`);
+
+        for (let i = 0; i < p.length; i++) {
+            p[i].remove();
+        }
+
+        let valores = Object.values(myLibrary[id]);
+        let llaves = Object.keys(myLibrary[id]);
+        
+        for (let j = 0; j < llaves.length; j++) {
+            let p = document.createElement('p');
+            p.className = `p${id}`;
+            div.appendChild(p);
+            p.innerHTML = `${llaves[j]}: <br> ${valores[j]}`
+        }
+    }
+}
+
+//Evento que llama a funcion para cambiar estado de libros.
+document.addEventListener('click', cambiarEstado );
+
+//Evento que llama a funcion para eliminar libros.
 document.addEventListener('click', eliminarLibro );
     
 //Muestra form para añadir libros.
